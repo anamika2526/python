398f9cc45380fda227f58ef75e7c0e0ebcfb277b
anamika2526 <anaa51gupta@gmail.com>
2026-03-18 09:28:15 +0530

    Create stack..py

---
+++ stack..py
@@ -0,0 +1,15 @@
+stack = []
+
+# push (add element)
+stack.append(10)
+stack.append(20)
+stack.append(30)
+
+print("Stack:", stack)
+
+# pop (remove element)
+stack.pop()
+print("After pop:", stack)
+
+# peek (top element)
+print("Top element:", stack[-1])
